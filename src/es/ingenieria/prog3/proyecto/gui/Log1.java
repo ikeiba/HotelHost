39beed327bd716af686a6f033bf1beddbf093dd6
Iker <ibarrola.iker@opendeusto.es>
2024-10-25 13:44:19 +0200

    Añadir preferred size y maximum size a los paneles para que la distancia entre ellos sea menor

--- src/es/ingenieria/prog3/proyecto/gui/Log1.java
+++ src/es/ingenieria/prog3/proyecto/gui/Log1.java
@@ -19,71 +19,102 @@ public class Log1 extends JPanel {
 		
 		
 		this.setLayout(new BorderLayout(10, 10));
+
+		// Crear el panel para el centro del JFrame
+		JPanel panelCentroLog = new JPanel(new GridLayout(1, 2, 10, 10));
 		
-        // Crear el panel para el centro del JFrame
-		JPanel centroLog = new JPanel(new GridLayout(1, 2, 10, 10));
         // Anadir el panel al centro del JFrame
-        this.add(centroLog, BorderLayout.CENTER);
+        this.add(panelCentroLog, BorderLayout.CENTER);
         
         // Anadir la etiqueta de copyright al south del panel
-        this.add(new JButton("HOLA"), BorderLayout.SOUTH);
+        this.add(new JLabel("Hotel Host® 2024"), BorderLayout.SOUTH);
         
         // Nuevo panel para la parte izquierda del log in (un BoxLayout)
-        JPanel izquierdaCentroLog = new JPanel();
-        izquierdaCentroLog.setLayout(new BoxLayout(izquierdaCentroLog, BoxLayout.Y_AXIS));
-        centroLog.add(izquierdaCentroLog);
+        JPanel panelIzquierdaCentroLog = new JPanel();
+        panelIzquierdaCentroLog.setLayout(new BoxLayout(panelIzquierdaCentroLog, BoxLayout.Y_AXIS));
+        panelCentroLog.add(panelIzquierdaCentroLog);
         
         // Nuevo panel para la parte derecha del log in (un BoxLayout)
-        JPanel derechaCentroLog = new JPanel();
-        derechaCentroLog.setLayout(new BoxLayout(derechaCentroLog, BoxLayout.Y_AXIS));
-        centroLog.add(derechaCentroLog);
+        JPanel panelDerechaCentroLog = new JPanel();
+        panelDerechaCentroLog.setLayout(new BoxLayout(panelDerechaCentroLog, BoxLayout.Y_AXIS));
+        panelCentroLog.add(panelDerechaCentroLog);
         
         // Creamos los JLabel para la parte izquierda        
         JLabel logo = new JLabel();
         ImageIcon icono = new ImageIcon("icono.png");
         logo.setIcon(icono);
-        JLabel descripcion = new JLabel("TEXTO LARGO Y ABURRIDO");
+        JLabel labelDescripcion = new JLabel("TEXTO LARGO Y ABURRIDO");
 
         // Centramos los diferentes labels
         logo.setAlignmentX(Component.CENTER_ALIGNMENT);
-        descripcion.setAlignmentX(Component.CENTER_ALIGNMENT);
+        labelDescripcion.setAlignmentX(Component.CENTER_ALIGNMENT);
         
-        izquierdaCentroLog.add(Box.createVerticalGlue());
-        izquierdaCentroLog.add(logo);
-        izquierdaCentroLog.add(Box.createVerticalStrut(50));
-        izquierdaCentroLog.add(descripcion);
-        izquierdaCentroLog.add(Box.createVerticalGlue());
+        panelIzquierdaCentroLog.add(Box.createVerticalGlue());
+        panelIzquierdaCentroLog.add(logo);
+        panelIzquierdaCentroLog.add(Box.createVerticalStrut(50));
+        panelIzquierdaCentroLog.add(labelDescripcion);
+        panelIzquierdaCentroLog.add(Box.createVerticalGlue());
         
         
-        // Creamos los JLabel para la parte izquierda        
-        JTextField login_usuario = new JTextField();
-        JTextField login_contraseña = new JTextField();
-        
-        login_usuario.setPreferredSize(new Dimension(300, 50));
-        login_usuario.setMaximumSize(new Dimension(300, 50));
-        login_usuario.setFont(new Font("Verdana", Font.PLAIN, 18));
-        
+		JPanel panelUsuario = new JPanel(new FlowLayout());
+		JPanel panelContrasena = new JPanel(new FlowLayout());
 
-        JButton boton_login = new JButton("LOG IN");
-        JButton boton_olvidar_contrasena = new JButton("¿Has olvidado la contraseña?");
+		Font fuenteTextField = new Font("Verdana", Font.BOLD, 18);
+		Font fuenteLabel = new Font("Verdana", Font.BOLD, 18);
+
+        
+        // Creamos los JLabel para la parte izquierda
+		JLabel labelUsuario = new JLabel("       Usuario: ");
+		labelUsuario.setFont(fuenteLabel);
+		
+        JTextField textFieldUsuario = new JTextField();
+        textFieldUsuario.setPreferredSize(new Dimension(300, 50));
+        textFieldUsuario.setMaximumSize(new Dimension(300, 50));
+        textFieldUsuario.setFont(fuenteTextField);
+
+		panelUsuario.add(labelUsuario);
+		panelUsuario.add(textFieldUsuario);
+		panelUsuario.setPreferredSize(new Dimension(500, 70));
+		panelUsuario.setMaximumSize(new Dimension(500, 70));
+
+		
+		JLabel labelContrasena = new JLabel("Contraseña: ");
+		labelContrasena.setFont(fuenteLabel);
+
+		JTextField textFieldContrasena = new JTextField();
+        textFieldContrasena.setPreferredSize(new Dimension(300, 50));
+        textFieldContrasena.setMaximumSize(new Dimension(300, 50));
+        textFieldContrasena.setFont(fuenteTextField);
+        
+        panelContrasena.add(labelContrasena);
+        panelContrasena.add(textFieldContrasena);
+        panelContrasena.setPreferredSize(new Dimension(500, 70));
+        panelContrasena.setMaximumSize(new Dimension(500, 70));
+
+		
+		
+
+       
+        JButton botonLogin = new JButton("LOG IN");
+        JButton botonOlvidarContrasena = new JButton("¿Has olvidado la contraseña?");
 
         
         // Centramos los diferentes labels
-        login_usuario.setAlignmentX(Component.CENTER_ALIGNMENT);
-        login_contraseña.setAlignmentX(Component.CENTER_ALIGNMENT);
-        boton_login.setAlignmentX(Component.CENTER_ALIGNMENT);
-        boton_olvidar_contrasena.setAlignmentX(Component.CENTER_ALIGNMENT);
+        textFieldUsuario.setAlignmentX(Component.CENTER_ALIGNMENT);
+        textFieldContrasena.setAlignmentX(Component.CENTER_ALIGNMENT);
+        botonLogin.setAlignmentX(Component.CENTER_ALIGNMENT);
+        botonOlvidarContrasena.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         
-        derechaCentroLog.add(Box.createVerticalGlue());
-        derechaCentroLog.add(login_usuario);
-        derechaCentroLog.add(Box.createVerticalStrut(50));
-        derechaCentroLog.add(login_contraseña);
-        derechaCentroLog.add(Box.createVerticalStrut(50));
-        derechaCentroLog.add(boton_login);
-        derechaCentroLog.add(Box.createVerticalStrut(50));
-        derechaCentroLog.add(boton_olvidar_contrasena);
-        derechaCentroLog.add(Box.createVerticalGlue());
+        panelDerechaCentroLog.add(Box.createVerticalGlue());
+        panelDerechaCentroLog.add(panelUsuario);
+        panelDerechaCentroLog.add(Box.createVerticalStrut(20));
+        panelDerechaCentroLog.add(panelContrasena);
+        panelDerechaCentroLog.add(Box.createVerticalStrut(20));
+        panelDerechaCentroLog.add(botonLogin);
+        panelDerechaCentroLog.add(Box.createVerticalStrut(20));
+        panelDerechaCentroLog.add(botonOlvidarContrasena);
+        panelDerechaCentroLog.add(Box.createVerticalGlue());
         
         
         //Hacemos lo mismo para la derecha
