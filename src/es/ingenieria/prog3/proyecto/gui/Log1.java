d04c49807f6fbab56d781159a8161b7f65277055
Iker <ibarrola.iker@opendeusto.es>
2024-10-29 13:30:41 +0100

    Actualizacion del log1 + creacion de GridFrame para referencias

--- src/es/ingenieria/prog3/proyecto/gui/Log1.java
+++ src/es/ingenieria/prog3/proyecto/gui/Log1.java
@@ -17,11 +17,11 @@ public class Log1 extends JPanel {
 		this.mainPanel = mainPanel;
 		
 		
-		
+		//El layout del panel sera un borderLayout
 		this.setLayout(new BorderLayout(10, 10));
 
 		// Crear el panel para el centro del JFrame
-		JPanel panelCentroLog = new JPanel(new GridLayout(1, 2, 10, 10));
+		JPanel panelCentroLog = new JPanel(new GridLayout(1, 2));
 		
         // Anadir el panel al centro del JFrame
         this.add(panelCentroLog, BorderLayout.CENTER);
@@ -29,68 +29,90 @@ public class Log1 extends JPanel {
         // Anadir la etiqueta de copyright al south del panel
         this.add(new JLabel("Hotel Host® 2024"), BorderLayout.SOUTH);
         
+        //Color de los paneles
+        Color colorPaneles = new Color(246, 233, 211);
+        
+        //PARTE IZQUIERDA
+        //--------------------------------------------------------------
         // Nuevo panel para la parte izquierda del log in (un BoxLayout)
-        JPanel panelIzquierdaCentroLog = new JPanel();
-        panelIzquierdaCentroLog.setLayout(new BoxLayout(panelIzquierdaCentroLog, BoxLayout.Y_AXIS));
-        panelCentroLog.add(panelIzquierdaCentroLog);
+        JPanel panelIzquierdaCentro = new JPanel();
+        panelIzquierdaCentro.setLayout(new BoxLayout(panelIzquierdaCentro, BoxLayout.Y_AXIS));
         
-        // Nuevo panel para la parte derecha del log in (un BoxLayout)
-        JPanel panelDerechaCentroLog = new JPanel();
-        panelDerechaCentroLog.setLayout(new BoxLayout(panelDerechaCentroLog, BoxLayout.Y_AXIS));
-        panelCentroLog.add(panelDerechaCentroLog);
-        
-        // Creamos los JLabel para la parte izquierda        
+        // Creamos los JLabel para la parte izquierda y le anadimos el icono       
         JLabel logo = new JLabel();
-        ImageIcon icono = new ImageIcon("resources/images/icono.png");
+        ImageIcon icono = new ImageIcon("resources/images/Hotel Host 200x200.png");
         logo.setIcon(icono);
         JLabel labelDescripcion = new JLabel("TEXTO LARGO Y ABURRIDO");
 
+        
         // Centramos los diferentes labels
         logo.setAlignmentX(Component.CENTER_ALIGNMENT);
         labelDescripcion.setAlignmentX(Component.CENTER_ALIGNMENT);
         
-        panelIzquierdaCentroLog.add(Box.createVerticalGlue());
-        panelIzquierdaCentroLog.add(logo);
-        panelIzquierdaCentroLog.add(Box.createVerticalStrut(50));
-        panelIzquierdaCentroLog.add(labelDescripcion);
-        panelIzquierdaCentroLog.add(Box.createVerticalGlue());
+        //Colocamos los componenetes donde queremos en el panel
+        panelIzquierdaCentro.add(Box.createVerticalStrut(30));
+        panelIzquierdaCentro.add(logo);
+        panelIzquierdaCentro.add(Box.createVerticalStrut(30));
+        panelIzquierdaCentro.add(labelDescripcion);
+        panelIzquierdaCentro.setBackground(colorPaneles);
+        
+      //Poner el borde del layout (top, left, bottom, right, color)
+        panelIzquierdaCentro.setBorder(BorderFactory.createMatteBorder(140, 80, 150, 80, Color.WHITE));
+        panelCentroLog.add(panelIzquierdaCentro);
+        //--------------------------------------------------------------
+        //FIN PARTE IZQUIERDA 
         
         
+        // PARTE DERECHA
+        //--------------------------------------------------------------
+        // Nuevo panel para la parte derecha del log in (un BoxLayout)
+        JPanel panelDerechaCentro = new JPanel();
+        panelDerechaCentro.setLayout(new BoxLayout(panelDerechaCentro, BoxLayout.Y_AXIS));
+        
+        //Crear dos paneles (uno para el textfield de usuario y el otro para el de contraseña)
 		JPanel panelUsuario = new JPanel(new FlowLayout());
 		JPanel panelContrasena = new JPanel(new FlowLayout());
-
+		
+		//Crear las fuentes
 		Font fuenteTextField = new Font("Verdana", Font.BOLD, 18);
 		Font fuenteLabel = new Font("Verdana", Font.BOLD, 18);
 
-        
-        // Creamos los JLabel para la parte izquierda
-		JLabel labelUsuario = new JLabel("Usuario: ");
-		labelUsuario.setFont(fuenteLabel);
-		
+		//Crear el textField del usuario
         JTextField textFieldUsuario = new JTextField();
         textFieldUsuario.setPreferredSize(new Dimension(300, 50));
         textFieldUsuario.setMaximumSize(new Dimension(300, 50));
         textFieldUsuario.setFont(fuenteTextField);
-
+        
+        //Creamos los JLabel para la parte izquierda del TextField del usuario
+		JLabel labelUsuario = new JLabel("Usuario: ");
+		labelUsuario.setFont(fuenteLabel);
+		
+		//Anadir todo al panel del usuario y especificar las dimensiones y el fondo
 		panelUsuario.add(labelUsuario);
 		panelUsuario.add(textFieldUsuario);
 		panelUsuario.setPreferredSize(new Dimension(500, 70));
 		panelUsuario.setMaximumSize(new Dimension(500, 70));
-
+		panelUsuario.setBackground(colorPaneles);
 		
-		JLabel labelContrasena = new JLabel("Contraseña: ");
-		labelContrasena.setFont(fuenteLabel);
-
+		//Crear el textField del usuario
 		JPasswordField passwordFieldContrasena = new JPasswordField();
         passwordFieldContrasena.setPreferredSize(new Dimension(300, 50));
         passwordFieldContrasena.setMaximumSize(new Dimension(300, 50));
         passwordFieldContrasena.setFont(fuenteTextField);
+        
+        //Creamos los JLabel para la parte izquierda del passwordField de la contrasena
+		JLabel labelContrasena = new JLabel("Contraseña: ");
+		labelContrasena.setFont(fuenteLabel);
 
+		//Creamos un checkBox que nos permitira visualizar o dejar de visualizar el contenido de la contraseña
         JCheckBox checkBox = new JCheckBox();
+        //Creamos las imagenes y las añadimos (una para mostrar y otra para ocultar)
         ImageIcon  imagenNoVer = new ImageIcon("resources/images/imagenNoVer.png");
         ImageIcon imagenVer = new ImageIcon("resources/images/imagenVer.png");
+        checkBox.setBackground(colorPaneles);
         checkBox.setIcon(imagenNoVer);
         checkBox.setSelectedIcon(imagenVer);
+        //Anadimos el listener para que si el checkbox esta seleccionado muestre la contraseña y en caso contrario la oculte
         checkBox.addActionListener(e -> {
         	if (checkBox.isSelected())
         		passwordFieldContrasena.setEchoChar((char) 0);
@@ -99,17 +121,20 @@ public class Log1 extends JPanel {
 
         });
         
+		//Anadir todo al panel de la contrasena y especificar las dimensiones y el fondo
         panelContrasena.add(labelContrasena);
         panelContrasena.add(passwordFieldContrasena);
         panelContrasena.add(checkBox);
         panelContrasena.setPreferredSize(new Dimension(600, 90));
         panelContrasena.setMaximumSize(new Dimension(600, 90));
-
+        panelContrasena.setBackground(colorPaneles);
 		
-		
 
-       
+       //Creamos y modificamos los botones de login y de haber olvidado la contrasena
         JButton botonLogin = new JButton("LOG IN");
+        botonLogin.setPreferredSize(new Dimension(150, 40));  // Ajusta el tamaño preferido
+        botonLogin.setMaximumSize(new Dimension(150, 40)); 
+
         JButton botonOlvidarContrasena = new JButton("¿Has olvidado la contraseña?");
 
         
@@ -118,20 +143,24 @@ public class Log1 extends JPanel {
         passwordFieldContrasena.setAlignmentX(Component.CENTER_ALIGNMENT);
         botonLogin.setAlignmentX(Component.CENTER_ALIGNMENT);
         botonOlvidarContrasena.setAlignmentX(Component.CENTER_ALIGNMENT);
-
         
-        panelDerechaCentroLog.add(Box.createVerticalGlue());
-        panelDerechaCentroLog.add(panelUsuario);
-        panelDerechaCentroLog.add(Box.createVerticalStrut(20));
-        panelDerechaCentroLog.add(panelContrasena);
-        panelDerechaCentroLog.add(Box.createVerticalStrut(20));
-        panelDerechaCentroLog.add(botonLogin);
-        panelDerechaCentroLog.add(Box.createVerticalStrut(20));
-        panelDerechaCentroLog.add(botonOlvidarContrasena);
-        panelDerechaCentroLog.add(Box.createVerticalGlue());
+        //Colocamos los componenetes donde queremos en el panel
+        panelDerechaCentro.add(Box.createVerticalStrut(50));
+        panelDerechaCentro.add(panelUsuario);
+        panelDerechaCentro.add(Box.createVerticalStrut(20));
+        panelDerechaCentro.add(panelContrasena);
+        panelDerechaCentro.add(Box.createVerticalStrut(0));
+        panelDerechaCentro.add(botonLogin);
+        panelDerechaCentro.add(Box.createVerticalStrut(30));
+        panelDerechaCentro.add(botonOlvidarContrasena);
         
         
-        //Hacemos lo mismo para la derecha
+      //Poner el borde del layout (top, left, bottom, right, color)
+        panelDerechaCentro.setBorder(BorderFactory.createMatteBorder(140, 40, 150, 40, Color.WHITE));
+        panelDerechaCentro.setBackground(new Color(246, 233, 211));
+        panelCentroLog.add(panelDerechaCentro);
+      //--------------------------------------------------------------
+      //FIN PARTE DERECHA 
         
 	}
 		
--- src/es/ingenieria/prog3/proyecto/gui/MainFrame.java
+++ src/es/ingenieria/prog3/proyecto/gui/MainFrame.java
@@ -2,7 +2,6 @@ package es.ingenieria.prog3.proyecto.gui;
 
 
 import java.awt.CardLayout;
-import java.awt.Dimension;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -18,9 +17,9 @@ public class MainFrame extends JFrame {
         // Set up the main frame properties
         setTitle("Hotel Host");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setMinimumSize(new Dimension(1000, 600));
-        setSize(960, 600);
-        //setExtendedState(JFrame.MAXIMIZED_BOTH);
+        // el 60% de la pantalla
+        setSize(1152, 720); 
+        setResizable(false);
         setLocationRelativeTo(null);
 
         // Initialize CardLayout and main panel
