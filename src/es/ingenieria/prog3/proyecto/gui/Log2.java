deff822241677108adb3dfb3e5b59a022ab1c79c
Enetz <enetz.q@opendeusto.es>
2024-10-30 11:26:09 +0100

    Log 2 Update

--- src/es/ingenieria/prog3/proyecto/gui/Log2.java
+++ src/es/ingenieria/prog3/proyecto/gui/Log2.java
@@ -26,55 +26,21 @@ public class Log2 extends JPanel {
 		
 		JLabel logo = new JLabel();
 		logo.setSize(new Dimension(200, 200));
-        ImageIcon icono = new ImageIcon("Hotel Host 200x200.png");
-        logo.setIcon(icono);
+		ImageIcon originalIcon = new ImageIcon("Hotel Host.png");
+		Image scaledImage = originalIcon.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+		ImageIcon resizedIcon = new ImageIcon(scaledImage);
+        logo.setIcon(resizedIcon);
         add(logo, BorderLayout.CENTER);
         
-        JLabel labelDescripcion = new JLabel("TEXTO LARGO Y ABURRIDO");
+        JLabel labelDescripcion = new JLabel("Aligned Text", JLabel.CENTER);
+        labelDescripcion.setHorizontalAlignment(SwingConstants.CENTER);
+        labelDescripcion.setVerticalAlignment(SwingConstants.TOP);
         labelDescripcion.setBounds(0, 0, 400, 50);
         add(labelDescripcion, BorderLayout.CENTER);
-		
-        VerticalLineExample linePanel = new VerticalLineExample();
-        linePanel.setBackground(panelCentro.getBackground());
-        linePanel.setOpaque(true);
-        linePanel.setBounds(0, 0, 10, 0);
-        add(linePanel, BorderLayout.CENTER);
         
 		add(panelCentro, BorderLayout.CENTER); //Añadimos el panel panelCentro al BorderLayout.CENTER del mainPanel
 		
         // Panel principal sur
         add(new JLabel("Hotel Host® 2024"), BorderLayout.SOUTH);
-        
-
-        // Add ComponentListener to resize components within the button panel based on frame size
-        addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentResized(ComponentEvent e) {
-                int width = getWidth();
-                int height = getHeight();
-                
-                // Resize and position the button within the buttonPanel
-                logo.setBounds((int) (width * 0.25) - (logo.getWidth() / 2), (int) (height * 0.35) - (logo.getHeight() / 2), logo.getWidth(), logo.getHeight());
-
-                // Resize and reposition the vertical line
-                linePanel.setBounds((int) (width * 0.5) - (linePanel.getWidth() / 2), (int) (height * 0.05), linePanel.getWidth(), (int) (height * 0.9)); // Fixed width, fills panel height
-            
-                // Resize and reposition the vertical line
-                labelDescripcion.setBounds((int) (width * 0.25) - (labelDescripcion.getWidth() / 2), (int) logo.getX() + (logo.getHeight() / 2) + 30, labelDescripcion.getWidth(), labelDescripcion.getHeight()); // Fixed width, fills panel height 
-            }
-        });
-    }
-
-    // Inner class for the vertical line
-    public static class VerticalLineExample extends JPanel {
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            g.setColor(Color.BLACK); // Set line color
-
-            // Draw a vertical line filling the entire height of the panel
-            int x = getWidth() / 2;
-            g.drawLine(x, 0, x, getHeight());
-        }
     }
 }
