4c2062ca457f8188977d91d705a99d17e3cb7b0b
Iker <ibarrola.iker@opendeusto.es>
2024-11-01 13:11:06 +0100

    Crear clase hotel

---
+++ src/es/ingenieria/prog3/proyecto/domain/Hotel.java
@@ -0,0 +1,18 @@
+package es.ingenieria.prog3.proyecto.domain;
+
+import java.util.ArrayList;
+
+public class Hotel {
+    
+	private static int contador = 1;
+	private int id;
+	private String nombre;
+	//private ArrayList<Habitacion> habitaciones;
+	private String ciudad;
+	private int Estrellas;
+	//private ArrayList<Valoracion> valoraciones;
+	private String descripcion;
+	//private ArrayList<Plan> planes;
+	//private ArrayList<Reserva> reservas;
+	
+}
