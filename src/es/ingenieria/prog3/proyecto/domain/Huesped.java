673781260673822496a7bde15bca9fa818f95001
Carlos Firvida <carlos.firvida@opendeusto.es>
2024-11-01 13:37:58 +0100

    Clase Huesped

---
+++ src/es/ingenieria/prog3/proyecto/domain/Huesped.java
@@ -0,0 +1,12 @@
+package es.ingenieria.prog3.proyecto.domain;
+
+import java.util.ArrayList;
+
+public class Huesped {
+	private static int contador = 1;
+	private int id;
+	private String nombre, apellido;
+	private ArrayList<Rerserva> reservas;
+	private ArrayList<Valoracion> valoraciones;
+	
+}
