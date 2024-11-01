4fe252ebda5a9c0b30345164324a3b08f27bffc3
Iker <ibarrola.iker@opendeusto.es>
2024-11-01 13:38:12 +0100

    update

--- src/es/ingenieria/prog3/proyecto/domain/Hotel.java
+++ src/es/ingenieria/prog3/proyecto/domain/Hotel.java
@@ -6,13 +6,10 @@ import java.util.ArrayList;
 public class Hotel {
     
 	private static int contador = 1;
-	private int id;
-	private String nombre;
+	private int id, estrellas;
+	private String nombre, ciudad, descripcion;
 	//private ArrayList<Habitacion> habitaciones;
-	private String ciudad;
-	private int Estrellas;
 	//private ArrayList<Valoracion> valoraciones;
-	private String descripcion;
 	private ArrayList<Plan> planes;
 	//private ArrayList<Reserva> reservas;
 	 
