de859bd4c4a266bcf2bc878c7e400419a48c47a0
Carlos Firvida <carlos.firvida@opendeusto.es>
2024-11-01 13:28:24 +0100

    Clase Valoracion

---
+++ src/es/ingenieria/prog3/proyecto/domain/Valoracion.java
@@ -0,0 +1,11 @@
+package es.ingenieria.prog3.proyecto.domain;
+
+import java.text.DateFormat;
+
+public class Valoracion {
+	private DateFormat fecha;
+	private String comentario;
+	private int puntuacion;
+	private Huesped autor;
+	
+}
