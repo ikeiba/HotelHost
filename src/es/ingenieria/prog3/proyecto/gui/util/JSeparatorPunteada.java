93d335fdf9feae1ea0571199d87421de2f3d3525
Enetz <enetz.q@opendeusto.es>
2024-11-04 18:08:50 +0100

    Create JSeparatorPunteada

---
+++ src/es/ingenieria/prog3/proyecto/gui/util/JSeparatorPunteada.java
@@ -0,0 +1,27 @@
+package es.ingenieria.prog3.proyecto.gui.util;
+
+import javax.swing.*;
+import java.awt.*;
+
+@SuppressWarnings("serial")
+public class JSeparatorPunteada extends JSeparator {
+
+    public JSeparatorPunteada() {
+        super();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        // Set stroke to create long dots with spaces in between
+        g2d.setStroke(new BasicStroke(1.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 1.0f, new float[]{(float) (Preferences.WINDOWWIDTH * 0.01), (float) (Preferences.WINDOWWIDTH * 0.01)}, 0.0f));
+        g2d.setColor(getForeground());
+        
+        // Draw a dotted line
+        if (getOrientation() == JSeparator.HORIZONTAL) {
+            g2d.drawLine(0, getHeight() / 2, getWidth(), getHeight() / 2);
+        } else {
+            g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+        }
+    }
+}
