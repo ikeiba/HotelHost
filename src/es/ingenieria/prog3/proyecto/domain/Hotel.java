8c533b8d004a0f744ca2b560749a857d13055c20
Iker <ibarrola.iker@opendeusto.es>
2024-11-08 09:09:05 +0100

    Solucion warning clase hotel

--- src/es/ingenieria/prog3/proyecto/domain/Hotel.java
+++ src/es/ingenieria/prog3/proyecto/domain/Hotel.java
@@ -4,8 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 import java.util.Objects;
 
 public class Hotel {
