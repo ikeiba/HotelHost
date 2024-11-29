77e048532e0a3bbc2b8be7aba6d1646f18aed9bf
ikeiba <ibarrola.iker@opendeusto.es>
2024-11-29 20:41:17 +0100

    update

--- src/es/ingenieria/prog3/proyecto/gui/DialogReservar.java
+++ src/es/ingenieria/prog3/proyecto/gui/DialogReservar.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 import es.ingenieria.prog3.proyecto.domain.Habitacion;
 import es.ingenieria.prog3.proyecto.domain.Reserva;
 import es.ingenieria.prog3.proyecto.domain.TipoHabitacion;
+import es.ingenieria.prog3.proyecto.gui.util.DataStore;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -29,6 +30,9 @@ public class DialogReservar extends JDialog {
 	
 	public ArrayList<Habitacion> getHabitacionesPorFechas(ArrayList<Habitacion> habitaciones){
 		ArrayList<Habitacion> habitacionesDisponibles = new ArrayList<Habitacion>();
+		Date fechaEntradaSeleccionada = DataStore.getSelectedFechaInicio();
+		Date fechaSalidaSeleccionada = DataStore.getSelectedFechaFin();
+
 		for (Habitacion habitacion : habitaciones) {
 			ArrayList<Reserva> reservas = habitacion.getReservas();
 			Collections.sort(reservas);
@@ -36,7 +40,12 @@ public class DialogReservar extends JDialog {
 			for (Reserva reserva : reservas) {
 				Date fechaEntradaReserva = new Date(reserva.getFechaInicio());
 				Date fechaSalidaReserva = new Date(reserva.getFechaFin());
-					
+				if (fechaEntradaReserva.after(fechaSalidaSeleccionada)) {
+					habitacionesDisponibles.add(habitacion);
+					break;
+				} else {
+					//if (fechaEntradaSeleccionada.)
+				}
 			}
 			
 		}
