b5e6fcecf27889b81380f60ae5d370d953cbafcb
ikeiba <ibarrola.iker@opendeusto.es>
2024-11-30 17:24:54 +0100

    updates

--- src/es/ingenieria/prog3/proyecto/gui/DialogReservar.java
+++ src/es/ingenieria/prog3/proyecto/gui/DialogReservar.java
@@ -18,20 +18,31 @@ public class DialogReservar extends JDialog {
 
 	private ArrayList<Habitacion> habitacionesDisponibles;
 	private JComboBox<Habitacion> comboBoxHabitacionesDisponibles;
+	private Habitacion habitacionSeleccionada;
+	private int numeroHuespedes;
 	
 	public DialogReservar(Hotel hotel) {
-		JPanel panelReserva = new JPanel(new GridLayout(3,1));
-
+        JPanel panelReserva = new JPanel(new GridLayout(8, 1, 5, 5)); // Espaciado adicional
+        
+        JLabel labelTipoHabitacion = new JLabel("Selecciona el tipo de habitación:");
 		JComboBox<TipoHabitacion> comboBoxTipoHabitacion = new JComboBox<TipoHabitacion>(TipoHabitacion.values());			
-		
+        
+		JLabel labelHabitaciones = new JLabel("Selecciona la habitación:");
 		habitacionesDisponibles = getHabitacionesPorFechas(getHabitacionesPorTipo(hotel.getHabitaciones(), (TipoHabitacion) comboBoxTipoHabitacion.getSelectedItem()));
 		comboBoxHabitacionesDisponibles = new JComboBox<Habitacion>(habitacionesDisponibles.toArray(new Habitacion [0]));
 		
+        JLabel labelHuespedes = new JLabel("Incluye a los huéspedes (al menos 1):");
 		JComboBox<String> comboHuespedes = new JComboBox<>();
-		Habitacion habitacionSeleccionada = (Habitacion) comboBoxHabitacionesDisponibles.getSelectedItem();
-		for (int i = 1; i <= habitacionSeleccionada.getCapacidad(); i++) {
-			comboHuespedes.addItem("Huesped " + i);
+		habitacionSeleccionada = (Habitacion) comboBoxHabitacionesDisponibles.getSelectedItem();
+		numeroHuespedes = habitacionSeleccionada.getCapacidad();
+		for (int i = 1; i <= numeroHuespedes; i++) {
+			comboHuespedes.addItem(String.format("Huesped - %d", i));
 		}
+		
+        JLabel labelPrecio = new JLabel("PRECIO TOTAL:");
+        
+        JLabel labelPrecioTotal = new JLabel(String.format("%.2f €", habitacionSeleccionada.getPrecio())); // Precio inicial
+
 		
 		comboBoxTipoHabitacion.addActionListener(e -> {
 			habitacionesDisponibles.clear();
@@ -40,19 +51,48 @@ public class DialogReservar extends JDialog {
 			for (Habitacion habitacion : habitacionesDisponibles) {
 				comboBoxHabitacionesDisponibles.addItem(habitacion);
 			}
+			if (habitacionesDisponibles.size() == 0) {
+				comboBoxHabitacionesDisponibles.setEnabled(false);
+			}
+			habitacionSeleccionada = (Habitacion) comboBoxHabitacionesDisponibles.getSelectedItem();
+			long diferenciaMilisegundos = DataStore.getSelectedFechaFin().getTime() - DataStore.getSelectedFechaInicio().getTime();
+	        long diasDiferencia = diferenciaMilisegundos / (1000 * 60 * 60 * 24);
+	        System.out.println("Días entre las fechas: " + diasDiferencia);
 		});
 		
 		comboBoxHabitacionesDisponibles.addActionListener(e -> {
-			
+			if (comboBoxHabitacionesDisponibles.getSelectedItem() != null) {
+				habitacionSeleccionada = (Habitacion) comboBoxHabitacionesDisponibles.getSelectedItem();
+				int nuevoNumeroHuespedes = habitacionSeleccionada.getCapacidad();
+				
+				if (nuevoNumeroHuespedes != numeroHuespedes) {
+					if (nuevoNumeroHuespedes > numeroHuespedes) {
+						for (int i=numeroHuespedes+1; i<=nuevoNumeroHuespedes; i++) {
+							comboHuespedes.addItem(String.format("Huesped - %d", i));					
+						}
+					} else {
+						for (int i=numeroHuespedes; i>nuevoNumeroHuespedes; i--) {
+							comboHuespedes.removeItemAt(i-1);
+						}
+					}
+				}
+				numeroHuespedes = comboHuespedes.getItemCount();
+			}	
 		});
 		
 		
 		
 		
 		
-		panelReserva.add(comboBoxTipoHabitacion);
-		panelReserva.add(comboBoxHabitacionesDisponibles);
-		panelReserva.add(comboHuespedes);
+        panelReserva.add(labelTipoHabitacion);
+        panelReserva.add(comboBoxTipoHabitacion);
+        panelReserva.add(labelHabitaciones);
+        panelReserva.add(comboBoxHabitacionesDisponibles);
+        panelReserva.add(labelHuespedes);
+        panelReserva.add(comboHuespedes);
+        panelReserva.add(labelPrecio);
+        panelReserva.add(labelPrecioTotal);
+        
 		add(panelReserva);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setTitle(String.format("Reserva en el hotel '%s'", hotel.getNombre()));		
